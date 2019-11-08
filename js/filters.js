4bf6db546ed3030775cebf3670ed02b58b8c3167
Nayniv <tigra8606@gmail.com>
2019-11-08 23:18:34 +0300

    module7-task3

--- js/filters.js
+++ js/filters.js
@@ -6,26 +6,48 @@
   var filterRoom = document.querySelector('#housing-rooms');
   var filterPrice = document.querySelector('#housing-price');
   var filterGuest = document.querySelector('#housing-guests');
+  var featuresList = document.querySelector('#housing-features');
 
   var checkType = function (item) {
     return filterTypeOfHouse.value === 'any' || item.offer.type === filterTypeOfHouse.value;
   };
 
   var checkRooms = function (item) {
-    return filterRoom.value === 'any' || item.offer.rooms === filterRoom.value;
+    return filterRoom.value === 'any' || item.offer.rooms === Number(filterRoom.value);
   };
 
   var checkPrice = function (item) {
-    return filterPrice.value === 'any' || item.offer.price === filterPrice.value;
+    return filterPrice.value === 'any' || ((filterPrice.value === 'low') && (item.offer.price < 10000)) ||
+    ((filterPrice.value === 'middle') && (item.offer.price > 10000) && (item.offer.price < 50000)) ||
+    ((filterPrice.value === 'high') && (item.offer.price > 50000));
   };
 
   var checkGuest = function (item) {
-    return filterGuest.value === 'any' || item.offer.guests === filterGuest.value;
+    return filterGuest.value === 'any' || Number(filterGuest.value) !== 0 && item.offer.guests >= Number(filterGuest.value) ||
+    Number(filterGuest.value) === 0 && item.offer.guests === 0;
+  };
+
+  var getCheckedFeatures = function () {
+    var features = [];
+    featuresList.querySelectorAll('input[name=features]').forEach(function (feature) {
+      if (feature.checked) {
+        features.push(feature.value);
+      }
+    });
+
+    return features;
+  };
+
+  var checkFeatures = function (item) {
+    var checkedFeature = getCheckedFeatures();
+    return checkedFeature.every(function (feature) {
+      return item.offer.features.indexOf(feature) !== -1;
+    });
   };
 
   var filterAll = function () {
     return window.fullData.filter(function (item) {
-      return checkType(item) && checkRooms(item) && checkPrice(item) && checkGuest(item);
+      return checkType(item) && checkRooms(item) && checkPrice(item) && checkGuest(item) && checkFeatures(item);
     }).slice(0, PINS_COUNT);
   };
 
