2d4cfb5185ce8c3bc567f904e737dc8ccfab33b2
Nayniv <tigra8606@gmail.com>
2019-11-08 20:41:40 +0300

    module7-task3

--- js/filters.js
+++ js/filters.js
@@ -3,14 +3,29 @@
 (function () {
   var PINS_COUNT = 5;
   var filterTypeOfHouse = document.querySelector('#housing-type');
+  var filterRoom = document.querySelector('#housing-rooms');
+  var filterPrice = document.querySelector('#housing-price');
+  var filterGuest = document.querySelector('#housing-guests');
 
-  var getTypeOfHouse = function (item) {
+  var checkType = function (item) {
     return filterTypeOfHouse.value === 'any' || item.offer.type === filterTypeOfHouse.value;
+  };
+
+  var checkRooms = function (item) {
+    return filterRoom.value === 'any' || item.offer.rooms === filterRoom.value;
+  };
+
+  var checkPrice = function (item) {
+    return filterPrice.value === 'any' || item.offer.price === filterPrice.value;
+  };
+
+  var checkGuest = function (item) {
+    return filterGuest.value === 'any' || item.offer.guests === filterGuest.value;
   };
 
   var filterAll = function () {
     return window.fullData.filter(function (item) {
-      return getTypeOfHouse(item);
+      return checkType(item) && checkRooms(item) && checkPrice(item) && checkGuest(item);
     }).slice(0, PINS_COUNT);
   };
 
